fc0ae31a6654e6b8097f0a2594b87bad1ee48cf3
Leonardo <221110179@students.mikroskil.ac.id>
2024-04-14 17:26:03 +0700

    chore : update drawer style

--- lib/widgets/drawer_template.dart
+++ lib/widgets/drawer_template.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class DrawerTemplate extends StatelessWidget {
   const DrawerTemplate({super.key});
@@ -6,21 +7,116 @@ class DrawerTemplate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(color: Colors.blue),
-            child: Text("Drawer Header"),
-          ),
-          ListTile(
-            leading: Image.network(
-                "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png"),
-            title: const Text("Home"),
-            selected: true,
-          )
-        ],
+      child: Container(
+        color: Colors.white, // Set the background color here
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+              child: DrawerHeader(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipOval(
+                      child: Image.asset(
+                        "assets/images/profile/sule.jpeg",
+                        fit: BoxFit.cover,
+                        width: 60,
+                        height: 60,
+                      ),
+                    ),
+                    const SizedBox(height: 10,),
+                    const Text(
+                      'Sutisna', 
+                      style: TextStyle(
+                        height: 1,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600
+                      ),
+                    ),
+                    Text(
+                      'View profile', 
+                      style: TextStyle(fontSize: 14, color: Theme.of(context).textTheme.displayMedium!.color),
+                    ),
+                    const SizedBox(height: 16,),
+                    Row(
+                      children: [
+                        const Text(
+                          '30 ',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600
+                          ),
+                        ),
+                        Text(
+                          'profile viewers', 
+                          style: TextStyle(color: Theme.of(context).textTheme.displayMedium!.color),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const Expanded(
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text(
+                      "Groups",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      "Events",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Divider(),
+                ListTile(
+                  leading: Container(
+                    width: 20,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(231, 163, 62, 1),
+                      borderRadius: BorderRadius.circular(3), // You can adjust the radius value as needed
+                    ),
+                  ),
+
+                  title: const Text(
+                    'Try Premium for IDR0',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600
+                    ),
+                  )
+                ),
+                const ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text(
+                    'Settings',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600
+                    ),
+                  )
+                )
+              ],
+              )
+          ],
+        ),
       ),
     );
+
   }
 }
