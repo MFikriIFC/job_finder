475c5cfd7d6a4c1f56b6a11837b35af77d7a7dff
Fikri <221110816@students.mikrsokil.ac.id>
2024-04-27 11:36:32 +0700

    chore: implement provider state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:job_finder/models/bottom_nav_model.dart';
+import 'package:job_finder/pages/container_page.dart';
 import 'package:job_finder/pages/home_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => BNavModel(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,9 +24,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: "Myriad"),
-      home: const HomePage(),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(fontFamily: "Myriad"),
+        home: const ContainerPage());
   }
 }
