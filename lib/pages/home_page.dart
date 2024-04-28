4e508b82aae2eb13fdb840df77506180e034daea
Fikri <221110816@students.mikrsokil.ac.id>
2024-04-28 23:11:29 +0700

    chore: refactoring widgets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:job_finder/widgets/appbar_widget.dart';
 import 'package:job_finder/widgets/bottom_navbar.dart';
 import 'package:job_finder/widgets/card_timeline.dart';
 
@@ -13,66 +15,26 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        shape: const Border(bottom: BorderSide(color: Colors.grey, width: 2)),
-        leading: Builder(
-          builder: (context) => Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(100),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Text("Drawer Header"),
               ),
-              child: IconButton(
-                icon: Image.network(
-                  "https://media.licdn.com/dms/image/D5603AQHiurAb7d24ng/profile-displayphoto-shrink_800_800/0/1680149648357?e=1718236800&v=beta&t=yuxNamHnXL0Ka-gRv1htaGTbq03YMzqsEfoCfj42Crw",
-                  fit: BoxFit.contain,
-                ),
-                onPressed: () => Scaffold.of(context).openDrawer(),
-              ),
-            ),
+              ListTile(
+                leading: Image.network(
+                    "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png"),
+                title: const Text("Home"),
+                selected: true,
+              )
+            ],
           ),
         ),
-        title: TextField(
-          decoration: InputDecoration(
-            hintText: "Search",
-            prefixIcon: const Icon(Icons.search),
-            border: InputBorder.none,
-            fillColor: Colors.cyan[50],
-            filled: true,
-          ),
-        ),
-        actions: const [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0),
-            child: Icon(Icons.chat),
-          )
-        ],
-        centerTitle: true,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Text("Drawer Header"),
-            ),
-            ListTile(
-              leading: Image.network(
-                  "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png"),
-              title: const Text("Home"),
-              selected: true,
-            )
-          ],
-        ),
-      ),
-      bottomNavigationBar: const MyButtomNavBar(),
-      body: ListView(
-        children: const [
-          CardTimeLine(),
-        ],
-      ),
-    );
+        bottomNavigationBar: const MyButtomNavBar(),
+        body: const ScrollAppbar(
+          body: Column(children: const [CardTimeLine()]),
+        ));
   }
 }
