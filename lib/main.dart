74ea9182f974e93b89bdaebe5e485409253f4044
Leonardo <221110179@students.mikroskil.ac.id>
2024-07-03 21:35:03 +0700

    chore : update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:job_finder/models/bottom_nav_model.dart';
 import 'package:job_finder/models/pages_model.dart';
 import 'package:job_finder/models/post_models.dart';
 import 'package:job_finder/models/user_model.dart';
-import 'package:job_finder/pages/network/network_container.dart';
+import 'package:job_finder/pages/main_container.dart';
 import 'package:job_finder/models/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:job_finder/providers/chat_data_provider.dart';
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const NetworkContainer(),
+      home: const MainContainer(),
       theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
